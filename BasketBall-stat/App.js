650affcb335c3cc6551a3fe0db10a0277deb589b
Balint Dorner <dorner.balint@gmail.com>
2017-07-06 22:29:22 +0200

    feat(app): some text rendered

--- BasketBall-stat/App.js
+++ BasketBall-stat/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ReactNative from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>mivaaaan?</Text>
-      </View>
-    );
-  }
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
