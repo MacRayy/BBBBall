0267036db4847b05254a221e23df6a0f6ea02819
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 08:03:41 +0200

    feat(albumlist): albums displayed

--- AwesomeProject/src/components/albumList.js
+++ AwesomeProject/src/components/albumList.js
@@ -5,20 +5,25 @@ import axios from 'axios';
 // Class based component, for complex components
 class AlbumList extends Component {
     //default state for the component, when its created
-    //state is built in and setState below, as well
+    //state is built in concept and setState below, as well
     state = { albums: [] };
 
     // Will automatically be executed when component get rendered
     componentWillMount() {
         // Http request(axios)
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            // Update state with the response, setState re-renders the component
             .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
     } 
 
     render() {
         return (
             <View>
-                <Text>Album List!!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
