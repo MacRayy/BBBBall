c460156e9b1af8388b0347b25eaed1f2a4811667
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 07:25:50 +0200

    feat(albumlist): added componentWillMount method

--- AwesomeProject/src/components/albumList.js
+++ AwesomeProject/src/components/albumList.js
@@ -3,6 +3,11 @@ import { View, Text } from 'react-native';
 
 // Class based component, for complex components
 class AlbumList extends Component {
+    // Will automatically be executed when component get rendered
+    componentWillMount() {
+        console.log()
+    } 
+
     render() {
         return (
             <View>
