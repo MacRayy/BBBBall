f98b4e5a06eb5ef3ee7b0862e62669a1e1ea15a1
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 17:01:31 +0200

    works

--- BasketBallApp/App.js
+++ BasketBallApp/App.js
@@ -26,10 +26,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 20,
+    justifyContent: 'flex-start'
   },
 });
--- BasketBallApp/src/components/game.js
+++ BasketBallApp/src/components/game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, Image } from 'react-native';
 import axios from 'axios';
 
 export default class GameComponent extends React.Component {
@@ -9,27 +9,37 @@ export default class GameComponent extends React.Component {
             name: '',
             number: ''
         }
+        playerList = []
     }
 
     render() {
         return (
-            <View>
-                <Text>New Player</Text>
+            <View style={styles.view}>
+                <Text style={styles.navbar}>BASKETBALL STAT</Text>
                 <TextInput
                     onChangeText={(name) => this.setState({name})}
                     placeholder="Name"
+                    style={styles.input}
                     />
                 <TextInput
                     onChangeText={(number) => this.setState({number})}
                     placeholder="Number"
+                    style={styles.input}
                     />
-                {/*<Text>
-                    {this.state.name}
-                </Text>*/}
-                <Button
-                    title="Add new player"
-                    onPress={this.addNewPlayer.bind(this)}
-                ></Button>
+                <View style={styles.button}>
+                    <Button
+                        title="Add new player"
+                        
+                        onPress={this.addNewPlayer.bind(this)}
+                    ></Button>
+                </View>
+                <Text style={styles.teamTitle}>Team Members</Text>
+                <FlatList
+                    data={this.playerList}
+                    renderItem={({item}) =>
+                    <Text style={styles.listItems}>{item.number} - {item.name}</Text>}
+                    style={styles.list}
+                />
             </View>
         )
     }
@@ -37,8 +47,8 @@ export default class GameComponent extends React.Component {
     listPlayers() {
         axios.get('https://nickel-pheasant.glitch.me/players')
         .then (function (response) {
-            console.log(response.data);
-        })
+            this.playerList = response.data;
+        }.bind(this))
         .catch(function (error) {
             console.log(error);
         })
@@ -52,9 +62,52 @@ export default class GameComponent extends React.Component {
         })
         .then(function (response) {
             console.log(response.data);
-        })
+            this.listPlayers()
+        }.bind(this))
         .catch(function (error) {
             console.log(error);
         });
     }
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 20
+    },
+    view: {
+    },
+    navbar: {
+        textAlign: 'center',
+        padding: 10,
+        paddingTop: 20,
+        backgroundColor: 'orange',
+        fontWeight: 'bold',
+        fontSize: 20   
+    },
+    input: {
+        padding: 20,
+        fontSize: 16
+    },
+    button: {
+        marginLeft: 75,
+        width: 100
+    },
+    list: {
+        flex: 1,
+        padding: 10,
+        marginTop: 10,
+        width: 250
+    },
+    listItems: {
+        padding: 10,
+        fontSize: 16,
+        borderBottomWidth: 1,
+        borderColor: '#cccccc'
+
+    },
+    teamTitle: {
+        paddingTop: 15,
+        fontSize: 20,
+        textAlign: 'center',
+    }
+})
