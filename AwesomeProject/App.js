634cdd51bd735e41a41da539eac852c66265674f
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 07:08:17 +0200

    feat(albumlist): created component

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,17 +1,21 @@
 // Import libraries to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-import Header from 'src/components/header';
+import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
 
 
 // Create component
 const App = () => {
   return (
-    <Header headerText={'Albums'}/> // with headerText we give value to its child component
+    <View>
+      <Header headerText={'Albums'}/> // with headerText we give value to its child component
+      <AlbumList />
+    </View>
   );
 };
 
 
 // Render it to the device
-AppRegistry.registerComponent('albums', () => App)
+//AppRegistry.registerComponent('albums', () => App)
