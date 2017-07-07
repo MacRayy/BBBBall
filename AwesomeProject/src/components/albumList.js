21c7087c0a8ed7006ef942a4cdba29670dae29a3
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 08:09:23 +0200

    feat(albumdetail): component created

--- AwesomeProject/src/components/albumList.js
+++ AwesomeProject/src/components/albumList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
+import AlbumDetail from './albumDetail'
+
 // Class based component, for complex components
 class AlbumList extends Component {
     //default state for the component, when its created
@@ -17,7 +19,9 @@ class AlbumList extends Component {
     }
 
     renderAlbums() {
-        return this.state.albums.map(album => <Text>{album.title}</Text>);
+        return this.state.albums.map(album => 
+            <Text key={album.title}>{album.title}</Text>
+        );
     } 
 
     render() {
