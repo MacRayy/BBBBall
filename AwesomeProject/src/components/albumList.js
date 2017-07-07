6c2c05de6b0ffec0300c48e0cd9dd89de5099134
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 07:33:01 +0200

    feat(albumlist): axios httprequest created

--- AwesomeProject/src/components/albumList.js
+++ AwesomeProject/src/components/albumList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // Class based component, for complex components
 class AlbumList extends Component {
     // Will automatically be executed when component get rendered
     componentWillMount() {
-        console.log()
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
     } 
 
     render() {
