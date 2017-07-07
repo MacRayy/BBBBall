4012856dbb2bfa26eae26fc05515cf5928d0a8eb
Balint Dorner <dorner.balint@gmail.com>
2017-07-07 07:52:26 +0200

    feat(albumlist): default state is created

--- AwesomeProject/src/components/albumList.js
+++ AwesomeProject/src/components/albumList.js
@@ -4,10 +4,15 @@ import axios from 'axios';
 
 // Class based component, for complex components
 class AlbumList extends Component {
+    //default state for the component, when its created
+    //state is built in and setState below, as well
+    state = { albums: [] };
+
     // Will automatically be executed when component get rendered
     componentWillMount() {
+        // Http request(axios)
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
     } 
 
     render() {
